ab70b763c8656dfb750e5d0957fb35bccf19c90c
Rai <agentydragon@gmail.com>
2022-09-18 00:34:23 -0700

    Allow cell outputs under a threshold

--- nb_clear_output.py
+++ nb_clear_output.py
@@ -1,27 +1,37 @@
 import argparse
+import nbformat
 import os
-import subprocess
-import sys
+
+
+def process_file(filename, file_size_threshold, max_cell_size):
+    # Bail out early if the file is not large enough
+    if os.path.getsize(filename) <= file_size_threshold:
+        return
+
+    # Read the notebook
+    with open(filename) as f:
+        nb = nbformat.read(f, as_version=4)
+
+    # Clear output of cells that are too large
+    for cell in nb.cells:
+        if cell.cell_type == "code":
+            if sum(len(output.get("text", "")) for output in cell.outputs) > max_cell_size:
+                cell.outputs = []
+
+    # Write the notebook
+    with open(filename, "w") as f:
+        nbformat.write(nb, f)
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--file-size-threshold", type=int, default=10 * 1024)
+    parser.add_argument("--max-cell-size", type=int, default=1024)
     parser.add_argument("files", nargs="+")
     args = parser.parse_args()
 
-    filenames = [filename for filename in args.files if os.path.getsize(filename) > args.file_size_threshold]
-    try:
-        subprocess.check_call(
-            [sys.executable, "-m", "jupyter", "nbconvert", "--clear-output", "--inplace", *filenames],
-            env={"JUPYTER_NO_CONFIG": "please_use_default_config"},
-        )
-    except Exception:
-        for filename in filenames:
-            subprocess.check_call(
-                [sys.executable, "-m", "jupyter", "nbconvert", "--clear-output", "--inplace", filename],
-                env={"JUPYTER_NO_CONFIG": "please_use_default_config"},
-            )
+    for filename in args.files:
+        process_file(filename, args.file_size_threshold, args.max_cell_size)
 
 
 if __name__ == "__main__":
