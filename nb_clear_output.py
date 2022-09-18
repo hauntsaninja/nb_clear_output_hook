eac637be5638080ed22324d6db216a5c86d080e0
Rai <agentydragon@gmail.com>
2022-09-18 00:37:48 -0700

    Update nb_clear_output.py

--- nb_clear_output.py
+++ nb_clear_output.py
@@ -3,7 +3,7 @@ import nbformat
 import os
 
 
-def process_file(filename, file_size_threshold, max_cell_size):
+def process_file(filename, file_size_threshold, max_cell_output_size):
     # Bail out early if the file is not large enough
     if os.path.getsize(filename) <= file_size_threshold:
         return
@@ -14,9 +14,8 @@ def process_file(filename, file_size_threshold, max_cell_size):
 
     # Clear output of cells that are too large
     for cell in nb.cells:
-        if cell.cell_type == "code":
-            if sum(len(output.get("text", "")) for output in cell.outputs) > max_cell_size:
-                cell.outputs = []
+        if sum(len(output.get("text", "")) for output in cell.outputs) > max_cell_output_size:
+            cell.outputs = []
 
     # Write the notebook
     with open(filename, "w") as f:
@@ -26,12 +25,12 @@ def process_file(filename, file_size_threshold, max_cell_size):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--file-size-threshold", type=int, default=10 * 1024)
-    parser.add_argument("--max-cell-size", type=int, default=1024)
+    parser.add_argument("--max-cell-output-size", type=int, default=1024)
     parser.add_argument("files", nargs="+")
     args = parser.parse_args()
 
     for filename in args.files:
-        process_file(filename, args.file_size_threshold, args.max_cell_size)
+        process_file(filename, args.file_size_threshold, args.max_cell_output_size)
 
 
 if __name__ == "__main__":
