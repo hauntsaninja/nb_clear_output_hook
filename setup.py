1d59992a30dc4595eb9aa8f4219960212c623df3
hauntsaninja <hauntsaninja@gmail.com>
2022-09-21 17:57:37 -0700

    bump to 0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="nb_clear_output",
-    version="0.2.0",
+    version="0.3.0",
     py_modules=["nb_clear_output"],
     install_requires=["nbformat>=5"],
     entry_points={"console_scripts": ["nb_clear_output=nb_clear_output:main"]},
