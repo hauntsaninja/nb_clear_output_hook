1f52b851fed3ebe7920a496f196bbbdd180af8ea
hauntsaninja <hauntsaninja@gmail.com>
2022-09-21 17:15:39 -0700

    version bound

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name="nb_clear_output",
     version="0.2.0",
     py_modules=["nb_clear_output"],
-    install_requires=["nbformat"],
+    install_requires=["nbformat>=5"],
     entry_points={"console_scripts": ["nb_clear_output=nb_clear_output:main"]},
 )
